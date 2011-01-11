0daa2ae7e65aa7e54b43b19c8baae6a30a698d0a
Aaronontheweb <aaron@aaronstannard.com>
2011-01-10 23:52:03 -0800

    Added the GetSafeFileName utility method, which will take any string and turn it into a usuable filename. It doesn't return an extension, however.

---
+++ IsolatedStorageExtensions/IsolatedStorageHelper.Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Windows.Shapes;
+
+namespace IsolatedStorageExtensions
+{
+    public static partial class IsolatedStorageHelper
+    {
+        #region Filenaming Methods
+
+        public static string GetSafeFileName(string filename)
+        {
+            return System.IO.Path.GetInvalidPathChars().Aggregate(filename, (current, c) => current.Replace(c, '_'));
+        }
+
+        #endregion
+    }
+}
